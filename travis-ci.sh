6a1454770d0faa12166acdedc1c551ef3e8a3511
Хорев Алексей Андреевич <khorevaa@art-vizing.ru>
2018-04-17 16:00:02 +0300

    Тестирование на тревисе

---
+++ travis-ci.sh
@@ -0,0 +1,16 @@
+
+
+set -e
+echo "Устанавливаю версию OScript <$OSCRIPT_VERSION>"
+curl http://oscript.io/downloads/$OSCRIPT_VERSION/deb > oscript.deb 
+dpkg -i oscript.deb 
+rm -f oscript.deb
+
+opm install 1testrunner; 
+opm install 1bdd; 
+opm install notify
+opm install; 
+
+opm run coverage; 
+
+
